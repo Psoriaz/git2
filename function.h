3b8a43aecd1109adc3e35e962a84d324dec1ba6e
Ilya Sovetniy <sovetniy@bk.ru>
2023-12-26 06:39:42 +0300

    add 'add' and 'substract' functopn

---
+++ function.h
@@ -0,0 +1,6 @@
+//function.h
+#ifndef FUNCTION_H#define FUNCTION_H
+class Function {
+public:    static double add(double a, double b);
+    static double subtract(double a, double b);};
+#endif // FUNCTION_H
